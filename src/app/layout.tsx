0455812c198f7e4e388d8fc1d918adca2a645230
abdullahal5 <abdullahalfahin183@gmail.com>
2025-05-17 22:08:15 +0600

    redux initialized

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import Providers from "@/lib/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,10 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-bla`}>
-        <Toaster richColors position="top-center" />
-        {children}
-      </body>
+      <Providers>
+        <body className={`${inter.className} bg-bla`}>
+          <Toaster richColors position="top-center" />
+          {children}
+        </body>
+      </Providers>
     </html>
   );
 }
---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,9 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const baseApi = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://healthcare-server-pi.vercel.app/api/v1",
+  }),
+  endpoints: () => ({}),
+});
